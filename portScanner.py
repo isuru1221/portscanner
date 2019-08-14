6d98532095b531ff51145fb577e861c24476070a
isuru1221 <isurusandaruwann1221@gmail.com>
2019-08-15 00:24:12 +0530

    create port scanner:)

---
+++ portScanner.py
@@ -0,0 +1,16 @@
+#!usr/bin/python
+
+import socket 
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host = input("Enter host ip")
+port = int(input("Enter port "))
+
+def scan(port):
+	if sock.connect_ex((host,port)):
+		print("port" ,port , "is closed")
+	else:
+		print("port" ,port , "is opened")
+
+
+scan(port)
